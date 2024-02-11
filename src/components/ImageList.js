abd3595d87a48633fcfc069d6936479fbc922e1d
aryanankit <a.arya8279@gmail.com>
2024-02-11 21:12:38 +0530

    Fix the Image list comp

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ImageShow from './ImageShow';
 import './css/ImageList.css'
 
+//Image list component
 const ImageList = ({ images }) => {
     const renderedImages = images.map((image) => {
         return <ImageShow key={image.id} image={image} />
