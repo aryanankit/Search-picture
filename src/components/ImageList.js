757ce9047a944a2969ff2808fffd43bcb42512b2
aryanankit <a.arya8279@gmail.com>
2024-02-13 23:54:26 +0530

    Some layout changes

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ImageShow from './ImageShow';
 import './css/ImageList.css'
 
-//Image list component
 const ImageList = ({ images }) => {
     const renderedImages = images.map((image) => {
         return <ImageShow key={image.id} image={image} />
